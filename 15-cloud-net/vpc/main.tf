8c49c0723c006d854385fa003aa78c10f650752a
fedinly <fedinly@gmail.com>
2026-09-03 22:45:06 +0700

    Update main.tf

--- 15-cloud-net/vpc/main.tf
+++ 15-cloud-net/vpc/main.tf
@@ -7,20 +7,21 @@ terraform {
   required_version = ">=1.8.4"  ### some test 29.10.2025
 }
 
-resource "yandex_vpc_network" "cloud_net" {
+resource "yandex_vpc_network" "cloud_net1" {
   name           = var.vpc_name
+  folder_id      = var.folder_id
+  #cloud_id       = var.cloud_id
 }
 
-resource "yandex_vpc_subnet" "vpc_subn_1" {
-  name           = "public"
-  network_id     = yandex_vpc_network.cloud_net.id
-  v4_cidr_blocks = [var.cidr_blocks[0]]
-  zone           = var.subnet_zones[0]
-}
+resource "yandex_vpc_subnet" "cloud_subnets" {
+  for_each = {
+    "public" = "ru-central1-d"
+    "private" = "ru-central1-b"
+  }
 
-resource "yandex_vpc_subnet" "vpc_subn_2" {
-  name           = "private"
-  network_id     = yandex_vpc_network.cloud_net.id
-  v4_cidr_blocks = [var.cidr_blocks[1]]
-  zone           = var.subnet_zones[1]
+  name           = "${each.key}"
+  zone           = each.value
+  network_id     = yandex_vpc_network.cloud_net1.id
+  v4_cidr_blocks = ["192.168.${each.key == "public" ? 10 : 20}.0/24"]
+  #route_table_id = yandex_vpc_route_table.nat-instance-route.id
 }
